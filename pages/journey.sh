b39a8a9c0b1205d76bfe5c164a341ef714768f9d
Hyperling <me@hyperling.com>
2022-10-19 07:03:57 -0500

    Add initial content. Covers most the story. Not done yet though.

--- pages/journey.sh
+++ pages/journey.sh
@@ -12,9 +12,142 @@ cat << EOF
 			<h1 class="col-12 title">
 				My Journey
 			</h1>
-			<p class="col-12 text">
-				[ TBD, check back soon. ]
-			</p>
+		</div>
+		<div class="row">
+			<div class="col-12 text">
+				<strong>
+					[ WARNING: 
+						This content is still In Progress and needs a full review. 
+					  There may be typos, out of order content, and missing content.
+					]
+				</strong>
+				<p>
+					Headaches, depression, digestive issues, lack of energy, and eventually
+					unprovoked anxiety/panic attacks. This was the norm for me for most of
+					my life. I ate a standard diet including fast food with meat always 
+					being the whether going out or cooking at home.
+				</p>
+				<p>
+					Eventually I became lactose intolerant due to antibiotics but that was
+					only a minor inconvenience, and did not always stop me from consuming
+					things anyways. I had tried the pills which are supposed to help but
+					didn't notice any difference. This didn't phase my diet though, just
+					had to ask for "no cheese" and stop drinking milkshakes.
+				</p>
+				<p>
+					What finally hit me hard enough to start changing things was the panic
+					attacks and an itchy mole that had to be surgically removed. By this point
+					in my early 20s I was already plenty skeptical of things like organized
+					religion and businesses profitting off of people, but it started to occur
+					to me that there were not many facts about health in my knowledge bank.
+					What most people were doing and what I was doing is not working.
+				</p>
+				<p>
+					When I looked in the mirror to see my eyes dilating frantically and body
+					feeling shaky and nervous when simply resting or eating a meal I knew that
+					something was wrong. Then it occurred to me: "Why would God or Evolution 
+					ever produce a species that thinks of itself as so advanced, but has
+					so many health issues that they consistently die prematurely or suffer
+					chronically?" 
+				</p>
+				<p>
+					This lead me into a more nature-based approach to my lifestyle. Obvious
+					things such as soda and candy were elminated first. Next was fast food.
+					By the end of it I had eliminated "food" such as meat as well since I
+					admitted that if I was living in the wild, I wouldn't be able to kill
+					a cow with my bare hands or teeth, much less chew on its raw carcass. 
+				</p>
+				<p>
+					At this point my meals were mostly fruit and vegetables. I didn't really 
+					know what I was doing though. I still allowed myself bread, so most meals
+					were oatmeal or peanut butter sandwiches with either jelly or bananas. I 
+					was feeling	better, but knew that bread didn't grow on trees so I needed 
+					to keep going	further. This was around the time that a nasty looking and 
+					extremely itchy mole cropped had to be cut out of the the back of my leg.
+				</p>
+				<p>
+					My saving grace was quitting a restaurant job in order to seek a job
+					related to my upcoming career field of Computer Science. This was the 
+					summer of 2014 before my final year of school. I had figured out that my 
+					hydration was trash and was now drinking at least a gallon of water a day.
+					I didn't have luck finding an internship, but I had plenty of time to 
+					research health, and ended up doing an apple fast.
+				</p>
+				<p>
+					I gorged on apples. Multiple pounds a day for at least three days. I
+					honestly did it not expecting it to work. Being able to live on apples
+					seemed like a joke, but I was willing to try it as an experiment. To my
+					surprise I started feeling better and better! By the end I had decided
+					that there was power to this and needed to explore even more! 
+				</p>
+				<p>
+					For the following three months I was a 100% raw fruitarian eating as many
+					calories as I could. I followed the advice of many people, mostly vegan 
+					athletes such	as Durianrider and Freelee, but also others like John Kohler
+					and Dan "The Man" McDonald. Dan also provided lots of good advice on loving 
+					oneself and improving mental, emotional, and spiritual life. I also made 
+					sure what I was doing was backed by medical staff such as John McDougall.
+				</p>
+				<p>
+					I still tell people this to this day: those three months in 2014 were the 
+					best feeling of my life. My body felt light lightning. I had so much energy
+					and felt like I could literally accomplish anything. I started riding a bike
+					without having done it since I was a kid, and was able to ride to a town 
+					dozens of miles away with no training or practice, and didn't even feel
+					sore. My body was finally to the point that God or Evolution intended!!
+				</p>
+				<p>
+					Acne, gone. Skin was glowing and I got many compliments. Suffering from 
+					the heat and humidity of summer? Gone as well! My body was able to cool
+					off so much faster without having cooked food stuck in digestion. There
+					were so many weird things that people wouldn't think of being related to 
+					diet and lifestyle.
+				</p>
+				<p>
+					Speaking of lifestyle, that is something else I learned from listening to
+					my new role models. "Sleep Water Sugar" as Durianrider used to say. The 
+					PDF on my main Health section covers the 6 pillars that I found to be
+					highly important. "CTFU" (Carb The Fuck Up) is first for a reason, in my
+					experimentation it was the most important one. Water and sleep follow 
+					because I found my body does not operate or digest well if those are 
+					missing. Exercise is next because it can go longer without being kept up.
+					The last two are more spiritual, and reuire the first four to be maintained
+					in order for them to be beneficial, but once they are achieved then they
+					can help keep the first four in check when they dwindle.
+				</p>
+				<p>
+					There was a day where I wondered if I was now cured of something and could
+					go back to foods from my old diet. So one night for dinner I ate with my 
+					family and had some sort of pasta with meat. After eating I ran an errand, 
+					and remember within less than a few minutes, after only getting to the 
+					street after our home, I started nodding off at the wheel. What a quick
+					indication that certain foods are a no-go!
+				</p>
+				<p>
+					Why would I stop eating like this, you ask? This was a temporary situation.
+					I was on summer break from school and was free from a job. When the fall
+					semester started I needed a job again to pay for gas, books, etc. So I
+					added in clean RawTil4 type foods like rice. For many years I ate like this
+					and still felt great. No oil, which I still don't do to this day. Also no 
+					seasoning, I firmly believe in only eating food that you like the taste 
+					of alone and would be happy eating as a monomeal. This is one of many
+					reasons I don't like seasonings and prefer not to use them.
+				</p>
+				<p>
+					There are other things I was able to discover about my body as well, such
+					as alliums causing headaches, body pain, nightmares, and with garlic it
+					becomes full-on night terrors. These are an absolute no when I'm able 
+					to dictate it. Nightshades have also been slowly phased out, avoiding
+					all potato unless deep fried so that the poison is destroyed even things 
+					like tomatoes and non-spicy peppers. These are less of an issue, but raw 
+					and undercooked potatoes cause an obvious head and body tingle/ache, 
+					eggplant digests terribly, and tomatoes cause bloat. In 2022 as an 
+					experiment with my wife while she tried a low histamine experiment I 
+					also found that tomatoes were the reason that my nose has been running 
+					consistently for years! I had been blaming bad air, but just goes to 
+					show that you can never stop learning!
+				</p>
+			</div>
 		</div>
 EOF
 
