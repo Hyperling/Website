84d5ee306c8c52a9de0bfbf3533d0106f66859f1
Hyperling <me@hyperling.com>
2023-03-04 07:34:14 -0600

    Add a word.

--- pages/helpers/footer.php
+++ pages/helpers/footer.php
@@ -7,7 +7,7 @@
 			<h6 class="col-3 center">
 				<a href="https://github.com/Hyperling/www/blob/main/LICENSE" 
 					target="_blank" rel="noopener noreferrer"
-				>This website is free software! Click to learn more.</a>
+				>This website is free software! Click here to learn more.</a>
 			</h6>
 			<h6 class="col-3 center">
 				<a href="/sitemap/">Full Site Map</a>
