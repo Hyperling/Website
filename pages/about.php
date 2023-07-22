36904be18dfd149a628896c75016ce6f62fd7b4d
Hyperling <me@hyperling.com>
2023-07-22 08:08:23 -0700

    Trim spaces.

--- pages/about.php
+++ pages/about.php
@@ -5,31 +5,31 @@
 
 		<div class="row">
 			<h1 class="col-12 title">Who Am I?</h1>
-		</div>	
+		</div>
 		<div class="row">
 			<div class="col-12 text">
-				<p> 
+				<p>
 					Hi there! My name is Chad, I am the primary content creator behind the
 					Hyperling and HyperVegan brands. Thank you for your interest!
 				</p>
-				<p>	
-					My hobbies go further than just coding and video making. I am big into 
-					health and believe it is humanity's most important asset. I was fortunate 
-					to have time off school/work/hobbies in my early 20's and was able to 
-					lock in a great lifestyle after a life of chronic sickness. See more 
-					below in <a href="#health">My Health Protocol</a>. 
+				<p>
+					My hobbies go further than just coding and video making. I am big into
+					health and believe it is humanity's most important asset. I was fortunate
+					to have time off school/work/hobbies in my early 20's and was able to
+					lock in a great lifestyle after a life of chronic sickness. See more
+					below in <a href="#health">My Health Protocol</a>.
 					It also includes a link to my full history of changes.
 				</p>
-				<p>	
-					I am also an avid gardener, focusing on the principles of nature-based 
-					permaculture in order to grow fruits and vegetables, like in a Food 
-					Forest system. This comes with other outdoor interests such as hiking, 
-					camping, backpacking, foraging, and traveling. 
+				<p>
+					I am also an avid gardener, focusing on the principles of nature-based
+					permaculture in order to grow fruits and vegetables, like in a Food
+					Forest system. This comes with other outdoor interests such as hiking,
+					camping, backpacking, foraging, and traveling.
 				</p>
-				<p>	
+				<p>
 					As of 2023 I reside in the US state of Arizona.
 				</p>
-			</div>	
+			</div>
 		</div>
 
 <?php
