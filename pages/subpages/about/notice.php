7dfbaf2594672fb4a72a90aca5c1523066b2233c
Hyperling <me@hyperling.com>
2023-07-22 18:47:00 -0700

    Add more explicit message about stance on law vs Law.

--- pages/subpages/about/notice.php
+++ pages/subpages/about/notice.php
@@ -8,17 +8,26 @@
 				<strong>No Guarantee</strong>
 				<p>
 					Anything I say, do, or produce comes with no guarantee or warranty
-					unless explicitly noted.
+					unless explicitly noted. I have to potential to be wrong and strive
+					to admit and correct my mistakes. It is never my goal to cause harm.
 				</p>
 				<strong>Freedom of Choice</strong>
 				<p>
 					My time is my own and I choose what to do with it. I am in the process
-					of reclaiming any of it which I feel has been given away in error. 
+					of reclaiming any of it which I feel has been given away in error
+					or through misunderstood pretenses.
 				</p>
 				<strong>Freedom of Association</strong>
 				<p>
-					I choose who and what I do my business with and hold the right to make
-					different choices at any time.
+					I choose who and what I do my business with and hold the right to
+					make different choices at any time, regardless of any 3rd parties
+					outside the voluntary contract deciding whether it is permissable.
+				</p>
+				<strong>Legal Jurisdiction</strong>
+				<p>
+					I am in the process of learning the rules I have been mistakenly
+					bound by. Anything signed which gives away Natural Law rights will
+					be considered under duress through confusion and complication.
 				</p>
 			</div>
 		</div>
