a9d4eccffa155bc468f3e30d05105c9b6734ba4f
Chad <me@hyperling.com>
2023-07-23 01:07:13 +0000

    Formatting Enhancements, Other Improvements (#5)
    * Update location.
    
    * Trim spaces.
    
    * Pull the website in to a more readable size.
    
    * Remove TBDs for now, may not get to publishing them, but will focus on new apps being public to begin with.
    
    * Fix some weird app breaking issues by adding padding and doing the auto margins independently.
    
    * Remove the country "US" for "USA".

--- pages/about.php
+++ pages/about.php
@@ -5,51 +5,31 @@
 
 		<div class="row">
 			<h1 class="col-12 title">Who Am I?</h1>
-		</div>	
+		</div>
 		<div class="row">
 			<div class="col-12 text">
-				<p> 
+				<p>
 					Hi there! My name is Chad, I am the primary content creator behind the
 					Hyperling and HyperVegan brands. Thank you for your interest!
 				</p>
-				<p>	
-					My hobbies go further than just coding and video making. I am big into 
-					health and believe it is humanity's most important asset. I was fortunate 
-					to have time off school/work/hobbies in my early 20's and was able to 
-					lock in a great lifestyle after a life of chronic sickness. See more 
-					below in <a href="#health">My Health Protocol</a>. 
+				<p>
+					My hobbies go further than just coding and video making. I am big into
+					health and believe it is humanity's most important asset. I was fortunate
+					to have time off school/work/hobbies in my early 20's and was able to
+					lock in a great lifestyle after a life of chronic sickness. See more
+					below in <a href="#health">My Health Protocol</a>.
 					It also includes a link to my full history of changes.
 				</p>
-				<p>	
-					I am also an avid gardener, focusing on the principles of nature-based 
-					permaculture in order to grow fruits and vegetables, like in a Food 
-					Forest system. This comes with other outdoor interests such as hiking, 
-					camping, backpacking, foraging, and traveling. 
-				</p>
-				<!--
 				<p>
-					Currently I reside in the US state of Indiana and am working to move 
-					to the southwest, starting in Arizona. Colorado and Mexico will likely 
-					be explored. New Mexico and other locations will be visited as well 
-					but have not yet revealed areas which offer exactly what I'm looking 
-					for.
+					I am also an avid gardener, focusing on the principles of nature-based
+					permaculture in order to grow fruits and vegetables, like in a Food
+					Forest system. This comes with other outdoor interests such as hiking,
+					camping, backpacking, foraging, and traveling.
 				</p>
-				--> 
-				<p>	
-					As of 2022 I reside in the US state of Indiana and am working to move
-					to the southwest for the following reasons:
-				</p>
-				<ul>
-					<li>Dry Climate</li>
-					<li>Mountains</li>
-					<li>Lack of Bugs</li>
-					<li>Permaculture Possibilities</li>
-				</ul>
 				<p>
-					The search will begin in Arizona. Colorado and Mexico will likely
-					be explored. New Mexico and other locations will be visited as well.
+					As of 2023 I reside in Arizona, USA.
 				</p>
-			</div>	
+			</div>
 		</div>
 
 <?php
--- pages/subpages/home/apps.php
+++ pages/subpages/home/apps.php
@@ -17,10 +17,10 @@
 		<div class="row text center">
 			<div class="col-6" id="ctfu">
 				<figure>
-					<a href="https://play.google.com/store/apps/details?id=com.hyperling.carbupbeta" 
+					<a href="https://play.google.com/store/apps/details?id=com.hyperling.carbupbeta"
 						target="_blank" rel="noopener noreferrer">
 						<img width="100%" height="100%" alt="ctfu_image"
-							src="https://files.hyperling.com/media/ctfu.png" 
+							src="https://files.hyperling.com/media/ctfu.png"
 						/>
 						<figcaption>
 							<p>Carb Up! BETA</p>
@@ -28,58 +28,58 @@
 						<p>
 							Calculate cost-effectiveness of foods on a High Carb Low Fat lifestyle.
 						</p>
-						<p>	
-							[<a href="https://play.google.com/store/apps/details?id=com.hyperling.carbupbeta" 
+						<p>
+							[<a href="https://play.google.com/store/apps/details?id=com.hyperling.carbupbeta"
 								target="_blank" rel="noopener noreferrer">Play Store</a>]
 							[<a href="https://files.hyperling.com/apks/CarbUpBeta.apk">APK</a>]
 						</p>
-						<p>
+						<!--<p>
 							TBD:
 							<s>
 								[<a target="_blank" rel="noopener noreferrer">F-Droid</a>]
 								[<a target="_blank" rel="noopener noreferrer">Github</a>]
 							</s>
-						</p>
+						</p>-->
 					</figcaption>
 				</figure>
 			</div>
 
 			<div class="col-6" id="sleep">
 				<figure>
-					<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.the45minuterule" 
+					<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.the45minuterule"
 						target="_blank" rel="noopener noreferrer">
 						<img loading="lazy" width="100%" height="100%" alt="45minuterule"
 							src="https://files.hyperling.com/media/t45mr.png"
 						/>
 						<figcaption>
 							<p>45 Minute Rule</p>
-						</a> 
+						</a>
 						<p>
 							Determine when to go to bed if you'd like to wake up during light sleep.
 						</p>
 						<p>
-							[<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.the45minuterule" 
+							[<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.the45minuterule"
 								target="_blank" rel="noopener noreferrer">Play Store</a>]
 							[<a href="https://files.hyperling.com/apks/45MinuteRule.apk">APK</a>]
 						</p>
-						<p>
+						<!--<p>
 							TBD:
 							<s>
 								[<a target="_blank" rel="noopener noreferrer">F-Droid</a>]
 								[<a target="_blank" rel="noopener noreferrer">Github</a>]
 							</s>
-						</p>
+						</p>-->
 					</figcaption>
 				</figure>
 			</div>
-			
+
 			<div class="col-6" id="timer">
 				<figure>
-					<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.infinitetimer" 
+					<a href="https://play.google.com/store/apps/details?id=com.hyperling.apps.infinitetimer"
 						target="_blank" rel="noopener noreferrer"
 					>
 						<img loading="lazy" width="100%" height="100%" alt="infinitetimer_image"
-							src="https://files.hyperling.com/media/infinitetimer.png" 
+							src="https://files.hyperling.com/media/infinitetimer.png"
 						/>
 						<figcaption>
 							<p>Infinite Timer</p>
@@ -92,44 +92,44 @@
 								target="_blank" rel="noopener noreferrer">Play Store</a>]
 							[<a href="https://files.hyperling.com/apks/InfiniteTimer.apk">APK</a>]
 						</p>
-						<p>
+						<!--<p>
 							TBD:
 							<s>
 								[<a target="_blank" rel="noopener noreferrer">F-Droid</a>]
 								[<a target="_blank" rel="noopener noreferrer">Github</a>]
 							</s>
-						</p>
+						</p>-->
 					</figcaption>
 				</figure>
 			</div>
 
 			<div class="col-6" id="games">
 				<figure>
-					<a href="https://play.google.com/store/apps/details?id=apps.hyperling.com.platformer" 
+					<a href="https://play.google.com/store/apps/details?id=apps.hyperling.com.platformer"
 						target="_blank" rel="noopener noreferrer"
 					>
 						<img loading="lazy" width="100%" height="100%" alt="hypergames_image"
-							src="https://files.hyperling.com/media/hypergames.png" 
+							src="https://files.hyperling.com/media/hypergames.png"
 						/>
 						<figcaption>
 							<p>
 								HyperGames
 							</p>
-						</a> 
+						</a>
 						<p>
 							Began developing some games for fun. Not near a finished state, but "playable".
 						</p>
 						<p>
-							[<a href="https://play.google.com/store/apps/details?id=apps.hyperling.com.platformer" 
+							[<a href="https://play.google.com/store/apps/details?id=apps.hyperling.com.platformer"
 								target="_blank" rel="noopener noreferrer">Play Store</a>]
 							[<a href="https://files.hyperling.com/apks/HyperGames.apk">APK</a>]
 						</p>
-						<p>
+						<!--<p>
 							TBD:
 							<s>
 								[<a target="_blank" rel="noopener noreferrer">F-Droid</a>]
 								[<a target="_blank" rel="noopener noreferrer">Github</a>]
-							</s>
+							</s>-->
 						</p>
 					</figcaption>
 				</figure>
@@ -143,8 +143,8 @@
 			<div class="col-12 text">
 				<p>
 					For a full list of programs including my Ansible automation,
-					Docker setup, source code for this website, and fun projects 
-					like an obfuscating editor and music fixer, check out 
+					Docker setup, source code for this website, and fun projects
+					like an obfuscating editor and music fixer, check out
 					<a href="https://github.com/Hyperling" target="_blank">My Github</a>.
 				</p>
 			</div>
