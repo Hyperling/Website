342bc0247adf826812d1a059b1831d71aa5b41f3
Hyperling <me@hyperling.com>
2024-01-25 06:18:04 -0700

    Simplify text, add nomad note.

--- pages/about.php
+++ pages/about.php
@@ -9,27 +9,29 @@
 		<div class="row">
 			<div class="col-12 text">
 				<p>
-					Hi there! My name is Chad, I am the primary content creator behind the
-					Hyperling and HyperVegan brands. Thank you for your interest!
+					Hi there! My name is Chad, I'm the primary content creator
+					behind Hyperling and HyperVegan. Thank you for your interest!
 				</p>
 				<p>
-					My hobbies go further than just coding and video making. I am big into
-					health and believe it is humanity's most important asset. I was fortunate
-					to have time off school/work/hobbies in my early 20's and was able to
-					lock in a great lifestyle after a life of chronic sickness. See more
-					below in <a href="#health">My Health Protocol</a>.
+					My hobbies go further than coding and video making. I'm big
+					into health and believe it is humanity's most important asset. I
+					was fortunate in my early 20's and was able to heal myself and
+					lock in a great lifestyle after a life of chronic sickness. See
+					more below in <a href="#health">My Health Protocol</a>.
 					It also includes a link to my full history of changes.
 				</p>
 				<p>
-					I am also an avid gardener, focusing on the principles of nature-based
-					permaculture in order to grow fruits and vegetables, like in a Food
-					Forest system. This comes with other outdoor interests such as hiking,
-					camping, backpacking, foraging, and traveling.
+					I'm also an avid gardener, focusing on the principles of
+					nature-based permaculture in order to grow fruits and vegetables,
+					like in a Food Forest system. This comes with other outdoor
+					interests such as hiking, camping, backpacking, foraging, and
+					traveling.
 				</p>
 				<p>
-					I live within the boundaries known as the United States of America.
-					From birth to 2023 it was the subboundaries of Indiana, and since 2023
-					it is the subboundaries of Arizona.
+					I live within the boundaries known as the United States of
+					America.	From birth to 2023 it was the subboundaries of Indiana,
+					and since 2023 it is the subboundaries of Arizona. As of 2024 I
+					am working to realize my goal to be a nomad.
 				</p>
 			</div>
 		</div>
