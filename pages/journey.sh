08467399f2ff8647513684ce6f96aacdb56ec17a
Hyperling <me@hyperling.com>
2024-01-25 06:59:46 -0700

    Fix link including a space.

--- pages/journey.sh
+++ pages/journey.sh
@@ -112,8 +112,7 @@ cat << EOF
 					to my new role models. "Sleep Water Sugar" as Durianrider used to say.
 					The
 					<a href="https://files.hyperling.com/media/HealthPriorities.pdf"
-						target="_blank">PDF
-					</a>
+						target="_blank">PDF</a>
 					on my main
 					<a href="/about/#health" target="_blank">Health</a>
 					section covers the 6 pillars that I found to be highly important.
