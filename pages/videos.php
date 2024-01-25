d5985cd163114911d0609f688e45a2d61fec8a97
Hyperling <me@hyperling.com>
2024-01-25 06:15:30 -0700

    Add a link to Modern Day Wizards and my personal media. Simplify text.

--- pages/videos.php
+++ pages/videos.php
@@ -6,33 +6,54 @@
 	include "helpers/body_open.php";
 ?>
 
-		<div class="row">
-			<h1 class="col-12 title">Videos</h1>
-		</div>
-		<div class="row">
-			<div class="col-12 text">
-				<p>	
-					I enjoy making video content when I have free time. Other life duties 
-					take priority so this is not always frequent or often. For the best 
-					viewing experience, please go directly to my channel here:
-				</p>
-				<ul class="indent"><li>
-					<a href="https://odysee.com/@HyperVegan:2"
-						target="_blank" rel="noopener noreferrer"
-					>
-						https://odysee.com/@HyperVegan:2
-					</a>
-				</li></ul>
-				<p> I also have a separate channel for reposting content, found here:</p>
-				<ul class="indent"><li>
-					<a href="https://odysee.com/@HyperVegan-Reposts:5"
-						target="_blank" rel="noopener noreferrer"
-					>
-						https://odysee.com/@HyperVegan-Reposts:5
-					</a>
-				</li></ul>
-			</div>
-		</div>
+<div class="row">
+	<h1 class="col-12 title">Videos</h1>
+</div>
+<div class="row">
+	<div class="col-12 text">
+		<p>
+			For the best viewing experience, please go directly to my channel here:
+		</p>
+		<ul class="indent"><li>
+			<a href="https://odysee.com/@HyperVegan:2"
+				target="_blank" rel="noopener noreferrer"
+			>
+				Odysee | HyperVegan
+			</a>
+		</li></ul>
+		<p> I also have a separate channel for reposting content, found here:</p>
+		<ul class="indent"><li>
+			<a href="https://odysee.com/@HyperVegan-Reposts:5"
+				target="_blank" rel="noopener noreferrer"
+			>
+				Odysee | HyperVegan Reposts
+			</a>
+		</li></ul>
+		<p>There is also a backup of my content on Modern Day Wizards:</p>
+		<ul class="indent"><li>
+			<a href="https://moderndaywizards.org/c/hypervegan"
+				target="_blank" rel="noopener noreferrer"
+			>
+				Modern Day Wizards | HyperVegan
+			</a>
+		</li></ul>
+	</div>
+</div>
+
+<div class="row">
+	<h1 class="col-12 title">Personal Media</h1>
+</div>
+<div class="row">
+	<div class="col-12 text">
+		<p>
+			Ever wonder where the photos and videos that I take go since
+			I don't have social media? Try here!
+		</p>
+		<ul class="indent"><li>
+			<a href="/photos">My Photos</a>
+		</li></ul>
+	</div>
+</div>
 
 <?php
 	include "helpers/body_close.php";
