76716f5d7bb0925f48a7e251fb55e26d593c51da
Hyperling <me@hyperling.com>
2024-01-25 06:55:39 -0700

    Update wordiness and provide disclaimer on top rather than only for email.

--- pages/contact.php
+++ pages/contact.php
@@ -7,21 +7,29 @@
 			<h1 class="col-12 title">Contact Me</h1>
 		</div>
 		<div class="row">
+			<div class="col-12 header">
+				<p>
+					Timely responses are not guarenteed, as I spend a majority of
+					my time offline and enjoy traveling in signal-free zones.
+				</p>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-12 text">
 				<p>
-					Public inquiries can be made on my Odysee channel's discussion board.
+					Public comments and inquiries can be made on my Odysee channel's
+					discussion board. Feel free to drop in and say hello!
 				</p>
 				<ul class="indent"><li>
 					<a href="https://odysee.com/@HyperVegan:2?view=discussion"
 						target="_blank" rel="noopener noreferrer"
 					>
-						https://odysee.com/@HyperVegan:2?view=discussion
+						Odysee | HyperVegan Community
 					</a>
 				</li></ul>
 				<p>
-					For private matters, my email address may be used, but there is no
-					guarantee of a timely response. If I do not know of you then please be
-					sure to form the email in a way that does not look like spam.
+					My email address may be used for private matters. Please be sure
+					to form your email in a way which does not look like spam.
 				</p>
 				<ul class="indent"><li>
 					<a href="mailto:me@hyperling.com">me@hyperling.com</a>
