ae5c22999d0773412fa81dffeae95c1d00221ae0
Hyperling <me@hyperling.com>
2024-01-28 07:31:46 -0700

    Add more addresses and an automatic converter app.

--- pages/subpages/support/donate.php
+++ pages/subpages/support/donate.php
@@ -6,7 +6,7 @@
 		<div class="row">
 			<div class="col-12 text">
 				<p>
-					Monetary donations can be provided below through cryptocurrencies.
+					Donations can be provided directly to my crypto addresses.
 				</p>
 				<!--<ul class="indent"><li>
 					<table class="border">
@@ -31,16 +31,38 @@
 						</tbody>
 					</table>
 				</li></ul>-->
+			</div>
 
-				<strong><code>XMR | Monero</code></strong>
-				<ul class="indent"><li>
-					<kbd>4ATk6owoMki46CuVfyAHS57FB5deqVFudTsaifQC1cfmcaQemgPEftcjZcW9DmcyfrfdRjxHQ9m4JAVSorYTgm6h8JnT7ao</kbd>
-				</li></ul>
+			<div class="col-12 header">
+				<p>
+					<strong><code>XMR | Monero</code></strong>
+					<ul class="indent"><li>
+						<kbd>4ATk6owoMki46CuVfyAHS57FB5deqVFudTsaifQC1cfmcaQemgPEftcjZcW9DmcyfrfdRjxHQ9m4JAVSorYTgm6h8JnT7ao</kbd>
+					</li></ul>
 
-				<strong><code>LBC | LBRY/Odysee Credit</code></strong>
-				<ul class="indent"><li>
-					<kbd>bDWP6qZajtm9Q9EkryKTorRwKFd5eDbPJj</kbd>
-				</li></ul>
+					<strong><code>BTC | Bitcoin</code></strong>
+					<ul class="indent"><li>
+						<kbd>bc1qsfe8dkvry3d34kztz449gkq67wq8fu2nkgfkh0</kbd>
+					</li></ul>
 
+					<strong><code>LTC | Litecoin</code></strong>
+					<ul class="indent"><li>
+						<kbd>ltc1qavmpu5d6ljntxsd6jj548m4yys83zwscl0dzkx</kbd>
+					</li></ul>
+
+					<strong><code>LBC | LBRY/Odysee Credit</code></strong>
+					<ul class="indent"><li>
+						<kbd>bDWP6qZajtm9Q9EkryKTorRwKFd5eDbPJj</kbd>
+					</li></ul>
+				<p>
+			</div>
+
+			<div class="col-12 text">
+				<p>Or use the widget below to send me any coin you'd like!</p>
+			</div>
+
+			<div class="col-12 center header">
+				<!-- https://trocador.app/en/anonpayurlgenerator -->
+				<iframe src="https://trocador.app/anonpay/?ticker_to=xmr&network_to=Mainnet&address=4ATk6owoMki46CuVfyAHS57FB5deqVFudTsaifQC1cfmcaQemgPEftcjZcW9DmcyfrfdRjxHQ9m4JAVSorYTgm6h8JnT7ao&donation=True&amount=1337.0&name=Chad&description=Support+Chad+%40+Hyperling&email=me@hyperling,com&ticker_from=doge&network_from=Mainnet&buttonbgcolor=663399&textcolor=cccccc&bgcolor=" width="310" height="350" style="border:0" scrolling="no"></iframe>
 			</div>
 		</div>
