f55447e3f12a442768a57c9db1e7336b80a400ac
Hyperling <me@hyperling.com>
2024-03-06 12:01:16 -0700

    Add page for My Health Ptorocol.

---
+++ pages/health.php
@@ -0,0 +1,40 @@
+#!/usr/bin/php
+<!--
+	Page to provide ways people can support me.
+-->
+<?php
+	include "helpers/body_open.php";
+?>
+
+		<div class="row">
+			<h1 class="col-12 title">My Health Protocol</h1>
+		</div>
+		<div class="row">
+			<div class="col-12 text">
+				<p>
+					Please click the link below to download the PDF.
+				</p>
+				<ul class="indent"><li>
+					<a href="/files/media/HyperlingHealthProtocol.pdf">
+						Hyperling's Health Protocol [PDF]
+					</a>
+				</li></ul>
+				<p>
+					There is also an audio companion which can be found here:
+				</p>
+				<ul class="indent"><li>
+					<a href="">
+						Hyperling's Health Protocol [AUDIO] | Odysee
+					</a>
+				</li></ul>
+				<ul class="indent"><li>
+					<a href="">
+						Hyperling's Health Protocol [AUDIO] | Modern Day Wizards
+					</a>
+				</li></ul>
+			</div>
+		</div>
+
+<?php
+	include "helpers/body_close.php";
+?>
