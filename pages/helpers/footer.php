cfca3ae48177c3fb87990fb620288733f9db9b5f
Hyperling <me@hyperling.com>
2024-03-23 18:13:18 -0700

    Add a "help wanted?" type message on the bottom of the site.

--- pages/helpers/footer.php
+++ pages/helpers/footer.php
@@ -2,10 +2,20 @@
 <?php
 	include "menu.php"
 ?>
+		<div class="row">
+			<p><!-- Gap Area --></p>
+		</div>
+
 		<div class="row" id="footer">
+			<div class="col-12 text center">
+				Have a Health or Freedom related project which could use my help?
+				<a href="mailto:me@hyperling.com">Please let me know!</a>
+			</div>
+		</div>
+		<div class="row">
 			<div class="col-3"></div>
 			<h6 class="col-3 center">
-				<a href="https://github.com/Hyperling/www/blob/main/LICENSE" 
+				<a href="https://github.com/Hyperling/www/blob/main/LICENSE"
 					target="_blank" rel="noopener noreferrer"
 				>This website is free software! Click here to learn more.</a>
 			</h6>
