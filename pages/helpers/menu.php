399baaf4d72cd2d9a2615c4894b4946fa1cf55c5
Hyperling <me@hyperling.com>
2024-03-23 18:46:35 -0700

    Change menu item for home page to Apps.

--- pages/helpers/menu.php
+++ pages/helpers/menu.php
@@ -2,7 +2,7 @@
 		<div class="row header">
 			<ul class="menu-list">
 				<li class="col-1"></li>
-				<li class="col-2 menu-item"><a href="/home/">Home</a></li>
+				<li class="col-2 menu-item"><a href="/home/">Apps</a></li>
 				<li class="col-2 menu-item"><a href="/media/">Media</a></li>
 				<li class="col-2 menu-item"><a href="/about/">About</a></li>
 				<li class="col-2 menu-item"><a href="/contact/">Contact</a></li>
