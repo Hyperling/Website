5fcd93eda9585873da5d2b066a4aac9bc95363e5
Hyperling <me@hyperling.com>
2024-03-29 20:29:54 -0700

    Add a resume page.

---
+++ pages/resume.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<!--
+	Page for my resume and work skill links.
+-->
+<?php
+	include "helpers/body_open.php";
+?>
+
+		<div class="row">
+			<h1 class="col-12 title">Resume</h1>
+		</div>
+		<div class="row">
+			<div class="col-12 header center" >
+				<img src="/files/media/icons/home.jpg"> <!-- TBD use a new photo -->
+			</div>
+		</div>
+		<div class="row">
+			<h2 class="col-12 header">Current Status : Full Time</h2>
+			<div class="col-12 text">
+				<p>
+					Interested in hiring me? Please find my resume below!
+				</p>
+				<ul class="indent"><li>
+					<a href="https://hyperling.com/files/media/documents/Resume.pdf"
+						target="_blank" rel="noopener noreferrer"
+					>
+						[PDF] | Direct Download
+					</a>
+				</li></ul>
+				<ul class="indent"><li>
+					<a href="https://hyperling.com/files/media/documents/resume.md"
+						target="_blank" rel="noopener noreferrer"
+					>
+						[Markdown] | Direct Download
+					</a>
+				</li></ul>
+			</div>
+		</div>
+
+<?php
+	include "helpers/body_close.php";
+?>
