c2923dfb9e09333d587f3593432a00c28757d340
Hyperling <me@hyperling.com>
2024-03-31 18:16:20 -0700

    Add new announcement, add dates for them, and change how the anchors are displayed.

--- pages/helpers/advisory.php
+++ pages/helpers/advisory.php
@@ -8,8 +8,18 @@
 				<h2><i>Announcements</i></h2>
 			</div>
 			<div class="col-12 text">
-				<a href="/health/">
-					Find my free new eBook "Hyperling's Health Protocol" here!
-				</a>
+				2024-03-07
+				<br/>
+				Find my free new eBook, "Hyperling's Health Protocol",
+				<a href="/health/">here</a>!
+			</div>
+			<div class="col-12 text">
+				2024-03-31
+				<br/>
+				There's a
+				<a href="/home/#tictactoe">new app</a>
+				in town,
+				<a href="/files/apks/TicTacToe.apk">Tic-Tac-Toe</a>!
+
 			</div>
 		</div>
