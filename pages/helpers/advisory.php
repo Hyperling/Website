c00799a34f3faff20fec5c81ec23c6481b73b218
Hyperling <me@hyperling.com>
2024-05-16 09:32:46 -0700

    Remove TTT announcement.

--- pages/helpers/advisory.php
+++ pages/helpers/advisory.php
@@ -13,12 +13,12 @@
 				Find my free new eBook, "Hyperling's Health Protocol",
 				<a href="/health/">here</a>!
 			</div>
-			<div class="col-12 text">
+			<!--<div class="col-12 text">
 				<u>2024-03-31</u>
 				<br/>
 				There's a
 				<a href="/home/#tictactoe">new app</a>
 				in town,
 				<a href="/files/apks/TicTacToe.apk">Tic-Tac-Toe</a>!
-			</div>
+			</div>-->
 		</div>
